f4f80d1c919efa4ad8e140284315675c17a10909
Benjamin Flanders <flanderb@gmail.com>
2013-02-25 21:27:10 -0500

    added some methods to the class

--- fizzbuzzClassy.rb
+++ fizzbuzzClassy.rb
@@ -1,3 +1,5 @@
+require "json"
+
 class Fizzbuzz 
 	def self.fizzBuzz (begin_number, end_number)
 		(begin_number..end_number).map do |n|
@@ -12,7 +14,26 @@ class Fizzbuzz
 			end
 		end
 	end
+	
+	def self.text(begin_number, end_number)
+		fizzBuzz(begin_number, end_number).to_s
+	end
+		
+	def self.html(begin_number, end_number)
+		collector = "<ul>"
+		fizzBuzz(begin_number, end_number).each do |fb|
+			collector += "<li> #{fb} </li>"
+		end
+		collector += "</ul>"
+	end
+	
+	def self.json(begin_number, end_number)
+		JSON.generate( fizzBuzz(begin_number, end_number) )
+	end
+		
 end
 
-p Fizzbuzz.fizzBuzz(15, 15)
-
+p Fizzbuzz.fizzBuzz(1, 15)
+p Fizzbuzz.text(1, 15)
+p Fizzbuzz.html(1, 15)
+p Fizzbuzz.json(1, 15)
