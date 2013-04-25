4bf7c47c1ec30e45e9d86fb48510626000696988
flanderb <flanderb@gmail.com>
2013-04-25 14:33:43 -0400

    Added an exception being thrown if this has invalid arguments

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -25,7 +25,11 @@ class Fizzy
 	end
 	
 	def self.fizz_single(num)
-		if num % 15 == 0
+		if !num.is_a?(Numeric)
+			raise ArgumentError.new("The number must be a numeric value - currently = #{num}")
+		elsif num < 0
+			raise ArgumentError.new("The number must be non negitive = currently = #{num}")
+		elsif num % 15 == 0
 			 "FizzBuzz"
 		elsif num % 5 == 0
 			 "Buzz"
